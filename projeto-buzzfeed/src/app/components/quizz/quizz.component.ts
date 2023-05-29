ed8a38b013fe6199500680cd83c77ab26bfc2e89
VictorSR117 <victorvidas@hotmail.com>
2023-05-28 23:40:47 -0300

    buzzfeed basic logic done

--- projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -5,7 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
+
 export class QuizzComponent implements OnInit {
+  title: string = ""
+  questions: any
+  questionSelected: any
+
+  answers: string[] = []
+  answersSelected: string = ""
+
+  questionIndex: number = 0
+  questionMaxIndex: number = 0
+
+  finished: boolean = false
 
   constructor() { }
 
